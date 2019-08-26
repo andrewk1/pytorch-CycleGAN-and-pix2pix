fc9c668c0a3cf73cfbd9c2db7fd33f0591b3f727
Andrew Kondrich <andrewk1@stanford.edu>
2019-08-26 15:46:50 -0700

    add rcan model and dataloader

---
+++ data/rcan_dataset.py
@@ -0,0 +1,76 @@
+import os.path
+from data.base_dataset import BaseDataset, get_transform
+from data.image_folder import make_dataset
+from PIL import Image
+import random
+
+
+class RCANDataset(BaseDataset):
+    """
+    It requires two directories to host training images from domain A '/path/to/data/trainA'
+    and from domain B '/path/to/data/trainB' respectively.
+    You can train the model with the dataset flag '--dataroot /path/to/data'.
+    Similarly, you need to prepare two directories:
+    '/path/to/data/testA' and '/path/to/data/testB' during test time.
+    """
+
+    def __init__(self, opt):
+        """Initialize this dataset class.
+
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseDataset.__init__(self, opt)
+        self.dir_canonical = os.path.join(opt.dataroot, opt.phase + 'canonical')  # create a path '/path/to/data/trainA'
+        self.dir_random = os.path.join(opt.dataroot, opt.phase + 'random')  # create a path '/path/to/data/trainB'
+        self.dir_seg = os.path.join(opt.dataroot, opt.phase + 'seg')  # create a path '/path/to/data/trainB'
+
+        self.canonical_paths = sorted(make_dataset(self.dir_canonical, opt.max_dataset_size))   # load images from '/path/to/data/trainA'
+        self.random_paths = sorted(make_dataset(self.dir_random, opt.max_dataset_size))    # load images from '/path/to/data/trainB'
+        self.seg_paths = sorted(make_dataset(self.dir_seg, opt.max_dataset_size))    # load images from '/path/to/data/trainB'
+
+        self.canonical_size = len(self.canonical_paths)  # get the size of dataset A
+        self.random_size = len(self.random_paths)  # get the size of dataset B
+        self.seg_size = len(self.seg_paths)  # get the size of dataset B
+
+        assert canonical_size == random_size == seg_size, 'Dataset sizes are not the same'
+
+        input_nc = self.opt.output_nc if btoA else self.opt.input_nc       # get the number of channels of input image
+        output_nc = self.opt.input_nc if btoA else self.opt.output_nc      # get the number of channels of output image
+        self.transform_A = get_transform(self.opt, grayscale=False)
+        self.transform_B = get_transform(self.opt, grayscale=False)
+
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index (int)      -- a random integer for data indexing
+
+        Returns a dictionary that contains A, B, canonical_paths andrandom_paths 
+            A (tensor)       -- an image in the input domain
+            B (tensor)       -- its corresponding image in the target domain
+            canonical_paths (str)    -- image paths
+            random_paths (str)    -- image paths
+        """
+        canonical_path = self.canonical_paths[index % self.canonical_size]  # make sure index is within then range
+        random_path = self.random_paths[index % self.canonical_size]
+        seg_path = self.seg_paths[index % self.canonical_size]
+        
+        canonical_img = Image.open(canonical_path).convert('RGB')
+        random_img = Image.open(random_path).convert('RGB')
+        seg_img = Image.open(seg_path).convert('RGB')
+
+        # apply image transformation
+        canonical = self.transform_A(canonical_img)
+        random = self.transform_B(random_img)
+        seg = self.transform_B(seg_img)
+
+        return {'canonical': canonical, 'random': random, 'seg': seg}
+
+    def __len__(self):
+        """Return the total number of images in the dataset.
+
+        As we have two datasets with potentially different number of images,
+        we take a maximum of
+        """
+        return self.canonical_size
---
+++ models/rcan_model.py
@@ -0,0 +1,167 @@
+import torch
+import itertools
+from util.image_pool import ImagePool
+from .base_model import BaseModel
+from . import networks
+
+
+class RCANModel(BaseModel):
+    """
+    This class implements the CycleGAN model, for learning image-to-image translation without paired data.
+
+    The model training requires '--dataset_mode unaligned' dataset.
+    By default, it uses a '--netG resnet_9blocks' ResNet generator,
+    a '--netD basic' discriminator (PatchGAN introduced by pix2pix),
+    and a least-square GANs objective ('--gan_mode lsgan').
+
+    CycleGAN paper: https://arxiv.org/pdf/1703.10593.pdf
+    """
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+
+        Parameters:
+            parser          -- original option parser
+            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+
+        Returns:
+            the modified parser.
+
+        For CycleGAN, in addition to GAN losses, we introduce lambda_A, lambda_B, and lambda_identity for the following losses.
+        A (source domain), B (target domain).
+        Generators: G_A: A -> B; G_B: B -> A.
+        Discriminators: D_A: G_A(A) vs. B; D_B: G_B(B) vs. A.
+        Forward cycle loss:  lambda_A * ||G_B(G_A(A)) - A|| (Eqn. (2) in the paper)
+        Backward cycle loss: lambda_B * ||G_A(G_B(B)) - B|| (Eqn. (2) in the paper)
+        Identity loss (optional): lambda_identity * (||G_A(B) - B|| * lambda_B + ||G_B(A) - A|| * lambda_A) (Sec 5.2 "Photo generation from paintings" in the paper)
+        Dropout is not used in the original CycleGAN paper.
+        """
+        parser.set_defaults(no_dropout=True)  # default CycleGAN did not use dropout
+        if is_train:
+            parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
+            parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
+            parser.add_argument('--lambda_identity', type=float, default=0.5, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
+
+        return parser
+
+    def __init__(self, opt):
+        """Initialize the CycleGAN class.
+
+        Parameters:
+            opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseModel.__init__(self, opt)
+        # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
+        # TODO: Maybe add idt_A and pi_A as extensions
+        self.loss_names = ['pixel', 'seg', 'discrim']  # 'sem',  
+        # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
+        self.visual_names = ['random', 'canonical_pred', 'canonical', 'seg_pred', 'seg']
+
+        # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>.
+        if self.isTrain:
+            self.model_names = ['G_canonical', 'G_seg', 'D']  # G_sem', 
+        else:  # during test time, only load Gs
+            self.model_names = ['G_canonical']
+
+        self.netG_canonical = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
+                                        		not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+        self.netG_seg = networks.define_G(opt.output_nc, opt.input_nc, opt.ngf, opt.netG, opt.norm,
+                                          not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+
+        if self.isTrain:  # define discriminators
+            self.netD = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
+                                          opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids)
+
+        if self.isTrain:
+            if opt.lambda_identity > 0.0:  # only works when input and output images have the same number of channels
+                assert(opt.input_nc == opt.output_nc)
+
+            # TODO: Figure out if I should keep this image buffer
+            self.fake_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            # self.fake_B_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            # self.fake_B_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+
+            # define loss functions
+            self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)  # define GAN loss.
+            self.criterionCanonical = torch.nn.MSELoss()  # TODO: Figure out if this is the same as mean pairwise squared error
+            self.criterionSegmentation = torch.nn.L2Loss()
+
+            # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
+            self.optimizer_G_canonical = torch.optim.Adam(self.netG_canonical.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_G_seg = torch.optim.Adam(itertools.chain(self.netG_canonical.parameters(), self.netG_seg.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D = torch.optim.Adam(self.netD, lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers.append(self.optimizer_G_canonical)
+            self.optimizers.append(self.optimizer_G_seg)
+            self.optimizers.append(self.optimizer_D)
+
+    def set_input(self, input):
+        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+
+        Parameters:
+            input (dict): include the data itself and its metadata information.
+
+        The option 'direction' can be used to swap domain A and domain B.
+        """
+        self.canonical = input['canonical'].to(self.device)
+        self.random = input['random'].to(self.device)
+        self.seg = input['seg'].to(self.device)
+
+    def forward(self):
+        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        self.canonical_pred = self.netG_canonical(self.random)  # G_A(A)
+        self.seg_pred = self.netG_seg(self.canonical_pred)
+
+    def backward_D_basic(self, netD, real, fake):
+        """Calculate GAN loss for the discriminator
+
+        Parameters:
+            netD (network)      -- the discriminator D
+            real (tensor array) -- real images
+            fake (tensor array) -- images generated by a generator
+
+        Return the discriminator loss.
+        We also call loss_D.backward() to calculate the gradients.
+        """
+        # Real
+        pred_real = netD(real)
+        loss_D_real = self.criterionGAN(pred_real, True)
+        # Fake
+        pred_fake = netD(fake.detach())
+        loss_D_fake = self.criterionGAN(pred_fake, False)
+        # Combined loss and calculate gradients
+        loss_D = (loss_D_real + loss_D_fake) * 0.5
+        loss_D.backward()
+        return loss_D
+
+    def backward_D(self):
+        """Calculate GAN loss for discriminator D_A"""
+        fake_canonical = self.fake_pool.query(self.canonical_pred)
+        self.loss_D = self.backward_D_basic(self.netD, self.canonical, fake_canonical)
+
+    def backward_G(self):
+        """Calculate the loss for generators G_canonical and G_pred"""
+
+        # GAN loss D_A(G_A(A))
+        self.loss_discrim = self.criterionGAN(self.netD(self.canonical_pred), True)
+        self.loss_pixel = self.criterionCanonical(self.canonical_pred, self.canonical)
+        self.loss_seg = self.criterionSegmentation(self.seg_pred, self.seg)
+
+        # combined loss and calculate gradients
+        self.loss_G = self.loss_discrim + self.loss_pixel + self.loss_seg
+        self.loss_G.backward()
+
+    def optimize_parameters(self):
+        """Calculate losses, gradients, and update network weights; called in every training iteration"""
+        # forward
+        self.forward()      # compute fake images and reconstruction images.
+        # G_A and G_B
+        self.set_requires_grad([self.netD], False)  # Ds require no gradients when optimizing Gs
+        self.optimizer_G.zero_grad()  # set G_A and G_B's gradients to zero
+        self.backward_G()             # calculate gradients for G_A and G_B
+        self.optimizer_G.step()       # update G_A and G_B's weights
+        # D_A and D_B
+        self.set_requires_grad([self.netD], True)
+        self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
+        self.backward_D_A()      # calculate gradients for D_A
+        self.backward_D_B()      # calculate graidents for D_B
+        self.optimizer_D.step()  # update D_A and D_B's weights
